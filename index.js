d307d5df0fc043a8568ac4a4672ce8fbb634f7cf
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-01-16 20:43:25 +0300

    Completing Javascript scope lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Karen';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'John';//throws an error since unsuccessfully trying to reassign the least favourite customer
+}
